6c9a905b1348a9dc1c89a4f5ebc8b3a64a119b43
Joseph <jvg_0119@yahoo.com>
2016-09-26 14:34:17 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  
+  get 'index' => 'welcome#index'
+  get 'about' => 'welcome#about'
+
+#  root 'welcome#index'
+  root to: 'welcome#index'
+
+
+
+
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
