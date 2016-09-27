4ca554fb5ba875cae03978a8f5f46cc97bbaafd7
Joseph <jvg_0119@yahoo.com>
2016-09-27 11:08:23 -0700

    cp32 seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1.upto(50) do 
+	Post.create!(
+		title: Faker::Lorem.sentence,
+		body: Faker::Lorem.paragraph
+		)
+end
+posts = Post.all
+
+1.upto(100) do 
+	Comment.create!(
+		post: posts.sample,
+	#	post_id: posts.sample.id,   ## OK
+		body: Faker::Lorem.paragraph
+		)
+end
+
+puts "*".center(40,"*")
+puts
+puts "Done Seeding".center(40)
+puts "#{Post.count} posts were created.".center(40)
+puts "#{Comment.count} comments were created.".center(40)
+puts
+puts "*".center(40,"*")
+
+
+
+
+
+
+
+
+
+
+
