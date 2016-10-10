19f32b6890e81d6914e3aae5181d4b7c4f30eea2
Joseph <jvg_0119@yahoo.com>
2016-10-10 10:39:40 -0700

    cp42 validating posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,42 @@ module ApplicationHelper
 		"active" if current_page?(path)
 	end
 
+   def form_group_tag(errors, &block)
+     if errors.any?
+       content_tag :div, capture(&block), class: 'form-group has-error'
+     else
+       content_tag :div, capture(&block), class: 'form-group'
+     end
+   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
+
+
