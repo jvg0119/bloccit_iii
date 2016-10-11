4b1888334f0384dea15c6c07db076ac344ba4bb1
Joseph <jvg_0119@yahoo.com>
2016-10-11 09:48:37 -0700

    cp43 assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,20 @@ class Post < ApplicationRecord
 #	default_scope { order( created_at: 'desc'  ) } # OK 
 	default_scope { order('created_at DESC') }
 
+	def markdown_title
+		render_as_markdown(title)
+	end
+
+	def markdown_body
+		render_as_markdown(body)
+	end
+
+private
+	def render_as_markdown(markdown)
+		renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
+	   	extensions = {fenced_code_blocks: true, highlight: true}
+	   	redcarpet = Redcarpet::Markdown.new(renderer, extensions = {})
+	  	(redcarpet.render(markdown)).html_safe
+	  end
+
 end
