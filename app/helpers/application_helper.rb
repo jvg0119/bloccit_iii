aa6fee930ba9a1ad260a44ff6c5e6ca543440fad
Joseph <jvg_0119@yahoo.com>
2016-11-17 12:30:37 -0800

    cp53 assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,6 +29,37 @@ module ApplicationHelper
      end
    end
 
+   def up_vote_link_classes(post)
+   	 base = "glyphicon glyphicon-chevron-up "
+   	 vote = current_user.voted(post)  # this is a vote object but it could be nil (if not voted)
+ 	 # .down_vote?  is a vote method; you can call .up_vote? on a vote if there is a vote
+ 	 # if vote is nil you will error when you call the .up_vote? on it
+ 	 # to prevent from throwing a error (and stopping the app) use && like this
+ 	 # current_user.voted(post) && current_user.(post).up_vote?  
+ 	 # nil && nil.upvote?   will return   nil   instead of an error 
+
+# 	 test = vote && vote.up_vote? ? 'voted' : ''
+# 	 base + test
+
+ 	 # refactor to 
+	 base + (vote && vote.up_vote? ? 'voted' : '')
+   end
+
+   def down_vote_link_classes(post)
+   	 base = "glyphicon glyphicon-chevron-down "
+  	 vote = current_user.voted(post)
+   	 # base + (vote && vote.down_vote? ? 'voted' : '')
+   	 base += "voted" if vote && vote.down_vote?
+   	 base
+   end
+
 end
+
+
+# 	 base += vote && vote.up_vote?) ? 'voted' : '' if vote.up_vote?
+# 	 base + "voted" if vote && vote.up_vote?
+ 	 #base += "voted" if vote && vote.down_vote?
+# 	 base
+
 
 
